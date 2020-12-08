2cfe0484d11f6e618ad575202bcea2359586c8f3
kaan <kaan803@gmail.com>
2020-12-08 14:54:45 +0300

    graph representation and BFS

---
+++ graph/graph.go
@@ -0,0 +1,122 @@
+package main
+
+import (
+	"fmt"
+)
+
+// adjacency list representation of a graph data structure
+// vertices are denoted with positive integers (not zero!)
+// the actual structure uses hash maps for efficiency and ease of use
+// V is the set of keys E has
+type Graph struct {
+	E map[uint] map[uint] bool
+}
+
+// runs BFS starting from s and writes each discovered vertex to c
+// terminates when quit reads a value or when c no longer returns values`
+// assumes graph doesn't change while BFS executes
+func BFS(g *Graph, s uint, c, quit chan uint) {
+	discovered, parent := make(map[uint] bool), make(map[uint] uint)
+	queue := make([]uint, 0)
+	queue = append(queue, s)
+	discovered[s] = true
+
+	for len(queue) > 0 {
+		v := queue[0]
+		queue = queue[1:]
+
+		for u, b := range g.E[v] {
+			if b && !discovered[u] {
+				select {
+				case c <- u:
+					discovered[u] = true
+					queue = append(queue, u)
+					parent[u] = v
+				case <-quit:
+					close(c)
+					return
+				}
+			}
+		}
+	}
+	close(c)
+}
+
+func BFS_layer(g *Graph, s uint, l chan []uint, quit chan uint) {
+	discovered := make(map[uint] bool)
+	discovered[s] = true
+
+	L_prev := []uint{s}
+
+	for len(L_prev) > 0 {
+		L_new := make([]uint, 0)
+		for _, v := range L_prev {
+			for u, b := range g.E[v] {
+				if b && !discovered[u] {
+					discovered[u] = true
+					L_new = append(L_new, u)
+				}
+			}
+		}
+		select {
+		case l <- L_prev:
+			L_prev = L_new
+		case <-quit:
+			close(l)
+			return
+		}
+	}
+	close(l)
+}
+
+func Check_connected(g *Graph, s, t uint) bool {
+	c, quit := make(chan uint), make(chan uint)
+	go BFS(g, s, c, quit)
+	for v := range c {
+		fmt.Println(v)
+		if v == t {
+			return true
+		}
+	}
+	return false
+}
+
+func main() {
+	g := new(Graph)
+	g.E = map[uint] map[uint] bool {
+		1: map[uint] bool{
+			2: true,
+			3: true,
+		},
+		2: map[uint] bool{
+			1: true,
+			3: true,
+		},
+		3: map[uint] bool{
+			1: true,
+			2: true,
+			5: true,
+		},
+		5: map[uint] bool{
+			3: true,
+			100:true,
+		},
+		100: map[uint] bool{
+			5: true,
+			230:true,
+		},
+		230: map[uint] bool{
+			100:true,
+		},
+
+	}
+	if !Check_connected(g, 1, 230) {
+		fmt.Println("failed!")
+	}
+
+	l, q := make(chan []uint), make(chan uint)
+	go BFS_layer(g, 1, l, q)
+	for layer := range l {
+		fmt.Println(layer)
+	}
+}
