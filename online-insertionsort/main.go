b6327d0208d5523a859a816cb9e74d631c925bf9
kaan <kaan803@gmail.com>
2020-12-17 20:33:54 +0300

    change linear find with binary find for insertion sort

--- online-insertionsort/main.go
+++ online-insertionsort/main.go
@@ -4,15 +4,28 @@ import (
 	"fmt"
 )
 
+//TODO: also implement using a skip list which reduces runtime to O(n log(n)), see wikipedia page
+// alternatively, leaving gaps in the array (library sort/gapped insertion sort) reduces runtime to O(n*log(n))
+// with high probability, but this interferes with the "online" aspect. Another function to "oversee" the InsSort would be necessary to maintain "online" behavior while these changes happen
+
 // builds a sorted array as it receives values from c
 // returns the current array when caller reads from quit and ends routine
 func InsSort(c chan int, quit chan []int) {
 	a := []int{}
-	var j int
 	for {
 		select {
 		case x := <-c:
-			for j = 0; j < len(a) && a[j] < x; j++ {
+			var lo, hi, j int
+			for lo, hi = 0, len(a); lo < hi-1; {
+				if a[(lo+hi)/2] > x {
+					hi = (lo+hi)/2
+				} else {
+					lo = (lo+hi)/2
+				}
+			}
+			j = hi
+			if len(a) > 0 && a[0] > x { //edge-case x smaller than all elements
+				j = lo
 			}
 			a = append(a, 0)
 			copy(a[j+1:], a[j:len(a)-1])
@@ -35,12 +48,12 @@ func main() {
 			break
 		}
 	}
-	//	a := []int{6, 3, 12, 45, 92, 54, 21, -5, 98}
-	//	c, q := make(chan int), make(chan []int)
-	//	go InsSort(c, q)
-	//	for _, x := range a {
-	//		c <- x
-	//	}
+//	a := []int{6, 3, 12, 45, 92, 54, 21, -5, 98}
+//	c, q := make(chan int), make(chan []int)
+//	go InsSort(c, q)
+//	for _, x := range a {
+//		c <- x
+//	}
 	b := <-q
 	fmt.Println(b)
 }
