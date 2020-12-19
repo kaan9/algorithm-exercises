5c815a37bafb42ed958c4822c3b13f3c10f56553
kaan <kaan803@gmail.com>
2020-12-20 02:44:07 +0300

    started linkedlist impl in Go

---
+++ linked-list/linkedlist.go
@@ -0,0 +1,28 @@
+package linkedlist
+
+type node struct {
+	val uint
+	next *node
+}
+
+type List struct {
+	head *node
+}
+
+func (l *List) peek() {
+	return l.head.val
+}
+
+func (l *List) prepend(v uint) {
+	n := &node{v, l.head}
+	l.head = n
+}
+
+func (l *List) append(v uint) {
+	n := &node{v, nil}
+	next := &l.head
+	for (*next) != nil {
+		next = &(*next).next
+	}
+	*next = n
+}
